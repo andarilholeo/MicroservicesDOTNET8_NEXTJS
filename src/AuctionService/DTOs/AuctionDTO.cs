e8cdc0c002b38a5d1c26142f3010707a63b19579
Leonardo <leonardosantos2742@gmail.com>
2023-12-06 14:23:15 -0300

    criando DTO

---
+++ src/AuctionService/DTOs/AuctionDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace AuctionService.DTOs;
+
+public record AuctionDTO
+(
+    Guid Id,
+    int ReservePrice,
+    string Seller,
+    string Winner,
+    int? SoldAmount,
+    int? CurrentHighBid,
+    DateTime CreatedAt,
+    DateTime UpdatedAt,
+    DateTime AuctionEnd,
+
+
+    string Status
+);
